6645ce690457f3a2f9b0b9c0ca9ea2f6ef20ff3f
Lieba Natur Brilian <gingalibadeidara@gmail.com>
2025-02-23 04:49:53 +0700

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -21,7 +21,7 @@ import { remarkReadingTime } from "./src/plugins/remark-reading-time.mjs";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://naturbrilian.pages.dev",
+  site: "https://naturbrilian.vercel.app",
   base: "/",
   trailingSlash: "always",
   integrations: [
